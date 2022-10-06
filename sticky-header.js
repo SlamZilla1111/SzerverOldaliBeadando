323eebc19673d6046b8a085c917ff1b14a296de3
Birodalmix <gudmann.benjamin@gmail.com>
2022-10-06 15:13:32 +0200

    commit(warcrime)
    searchbar for index.html
    profil data for profil.html

---
+++ sticky-header.js
@@ -0,0 +1,30 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    header.classList.add("nomargin");
+  } else {
+    header.classList.remove("sticky");
+    header.classList.remove("nomargin");
+  }
+}
+//----------------------------------------password hide/show-------------------------------------------
+  const togglePassword = document.querySelector('#togglePassword');
+  const password = document.querySelector('#id_password');
+
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
