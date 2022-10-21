05a8360d0d18ab3ae780cc68fb46d772379ac448
Szoblerone <martin99komuves@gmail.com>
2022-10-21 12:49:30 +0200

    Admin kereső javítva

---
+++ adminsearch-bar.js
@@ -0,0 +1,18 @@
+document.querySelector('#search-input').addEventListener('input', filterList);
+
+function filterList() {
+    const searchbar=document.querySelector('#search-input');
+    const filter= searchbar.value.toLowerCase();
+    const listItems =document.querySelectorAll('.collapsible');
+
+    listItems.forEach((item)=>{
+        let text = item.textContent;
+        if (text.toLocaleLowerCase().includes(filter.toLocaleLowerCase())) {
+            item.style.display = '';
+        } else {
+            item.style.display='none';
+        }
+    });
+
+}
+
